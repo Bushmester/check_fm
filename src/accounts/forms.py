a6ad4ad9c40e9896fa1616c9d26fc219ba7aa882
Ilnar Gomelyanov <hibushland@gmail.com>
2021-10-27 19:22:45 +0300

    clean accounts form

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -1,20 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, validators
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class LoginForm(FlaskForm):
     username = StringField(
         "username",
         validators=[
-            validators.DataRequired(),
-            validators.Length(min=4, max=32)
+            DataRequired(),
+            Length(min=4, max=32)
         ]
     )
     password = PasswordField(
         "password",
         validators=[
-            validators.DataRequired(),
-            validators.Length(min=6, max=64)
+            DataRequired(),
+            Length(min=6, max=64)
         ]
     )
     remember = BooleanField("remember")
@@ -24,30 +25,30 @@ class SignupForm(FlaskForm):
     email = StringField(
         "email",
         validators=[
-            validators.DataRequired(),
-            validators.Email(),
-            validators.Length(min=6)
+            DataRequired(),
+            Email(),
+            Length(min=6)
         ]
     )
     username = StringField(
         "username",
         validators=[
-            validators.DataRequired(),
-            validators.Length(min=4, max=32)
+            DataRequired(),
+            Length(min=4, max=32)
         ]
     )
     password = PasswordField(
         "password",
         validators=[
-            validators.DataRequired(),
-            validators.Length(min=6, max=64),
-            validators.EqualTo(fieldname='confirm_password')
+            DataRequired(),
+            Length(min=6, max=64),
+            EqualTo(fieldname='confirm_password')
         ]
     )
     confirm_password = PasswordField(
         "confirm_password",
         validators=[
-            validators.DataRequired(),
-            validators.Length(min=6, max=64),
+            DataRequired(),
+            Length(min=6, max=64),
         ]
     )
